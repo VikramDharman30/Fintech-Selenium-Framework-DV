6963e770c0979e6c645eef737dd2260f9c500ff8
Vikram <dmvikramr505@gmail.com>
2026-04-11 09:16:21 +0530

    Improved account creation stability by waiting for dynamic dropdown values and Added Transfer Funds workflow with account creation dependency

---
+++ src/test/java/pages/TransferFundsPage.java
@@ -0,0 +1,119 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
+
+public class TransferFundsPage {
+
+    WebDriver driver;
+
+    public TransferFundsPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Locators
+
+    By transferFundsLink =
+            By.linkText("Transfer Funds");
+
+    By amountField =
+            By.id("amount");
+
+    By fromAccountDropdown =
+            By.id("fromAccountId");
+
+    By toAccountDropdown =
+            By.id("toAccountId");
+
+    By transferButton =
+            By.xpath("//input[@value='Transfer']");
+
+    By transferSuccessMessage =
+            By.xpath("//h1[contains(text(),'Transfer Complete')]");
+
+    // Actions
+
+    public void navigateToTransferFunds() {
+
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        wait.until(ExpectedConditions
+                        .elementToBeClickable(transferFundsLink))
+                .click();
+
+    }
+
+    public void enterAmount(String amount) {
+
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        wait.until(ExpectedConditions
+                        .visibilityOfElementLocated(amountField))
+                .sendKeys(amount);
+
+    }
+
+    public void selectFromAccount() {
+
+        Select select =
+                new Select(driver.findElement(fromAccountDropdown));
+
+        select.selectByIndex(0);
+
+    }
+
+    public void selectToAccount() {
+
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        wait.until(ExpectedConditions
+                .presenceOfElementLocated(toAccountDropdown));
+
+        Select select =
+                new Select(driver.findElement(toAccountDropdown));
+
+        if (select.getOptions().size() > 1) {
+
+            select.selectByIndex(1);
+
+        } else {
+
+            select.selectByIndex(0);
+
+        }
+
+    }
+
+    public void clickTransferButton() {
+
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        wait.until(ExpectedConditions
+                        .elementToBeClickable(transferButton))
+                .click();
+
+    }
+
+    public boolean isTransferSuccessful() {
+
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        wait.until(ExpectedConditions
+                .visibilityOfElementLocated(transferSuccessMessage));
+
+        return driver.findElement(transferSuccessMessage)
+                .isDisplayed();
+
+    }
+
+}
---
+++ src/test/java/tests/TransferFundsTest.java
@@ -0,0 +1,66 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.RegistrationPage;
+import pages.AccountsPage;
+import pages.TransferFundsPage;
+
+public class TransferFundsTest extends BaseTest {
+
+    @Test
+    public void transferFundsWorkflowTest() {
+
+        RegistrationPage registrationPage =
+                new RegistrationPage(driver);
+
+        String username =
+                "user" + System.currentTimeMillis();
+
+        String password = "password123";
+
+        // Register user (auto login)
+
+        registrationPage.registerUser(
+                username,
+                password
+        );
+
+        AccountsPage accountsPage =
+                new AccountsPage(driver);
+
+        // Create new account
+
+        accountsPage.clickOpenNewAccount();
+
+        accountsPage.selectAccountType();
+
+        accountsPage.clickOpenAccountButton();
+
+        accountsPage.isAccountCreated();
+
+        TransferFundsPage transferPage =
+                new TransferFundsPage(driver);
+
+        // Transfer funds
+
+        transferPage.navigateToTransferFunds();
+
+        transferPage.enterAmount("100");
+
+        transferPage.selectFromAccount();
+
+        transferPage.selectToAccount();
+
+        transferPage.clickTransferButton();
+
+        boolean status =
+                transferPage.isTransferSuccessful();
+
+        System.out.println(
+                "Transfer status: " + status);
+
+    }
+
+}
